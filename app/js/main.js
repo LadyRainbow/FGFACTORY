10c63fd02a8423797a408238365fad4802348b3a
LadyRainbow <m.trojchenko@gmail.com>
2016-12-12 00:11:51 +0200

    first factory commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+
+$('.corusel').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1
+});
---
+++ dist/js/plugin.js
@@ -0,0 +1,56 @@
+
+;(function ($){
+	var header = $('header');
+	var windowHeight = $(window).height();
+
+	$(window).on('scroll', function(){
+		var scrollTop = $(window).scrollTop();
+
+		if(scrollTop > windowHeight/2){
+			header.addClass('is-fixed');
+		}
+		else {
+			header.removeClass('is-fixed');
+		}
+	});
+})(jQuery);
+
+(function ($){
+	$(window).on('load', function(){
+		var prev = $('.slick-prev');
+		var next = $('.slick-next');
+
+		var parentCarousel, currentSlide, nextSlide, prevSlide;
+
+		function addClassSlider(self){
+			parentCarousel = $(self).closest('.corusel');
+			currentSlide = $(parentCarousel).find('.slick-current');
+
+			nextSlide = currentSlide.next('.slick-slide');
+			prevSlide = currentSlide.prev('.slick-slide');
+
+			if (elem = 'next') {
+				$(currentSlide).addClass('translate-left');
+				$(nextSlide).addClass('translate-left');
+			}	else {
+				$(currentSlide).addClass('translate-right');
+				$(prevSlide).addClass('translate-right');
+			}
+		};
+
+		next.on('mouseover', function(){
+			addClassSlider(this, 'next');
+		});
+		next.on('mouseout', function(){
+			$(currentSlide).removeClass('translate-left');
+			$(nextSlide).removeClass('translate-left');
+		});
+		prev.on('mouseover', function(){
+			addClassSlider(this, 'prev');
+		});
+		prev.on('mouseout', function(){
+			$(currentSlide).removeClass('translate-right');
+			$(prevSlide).removeClass('translate-right');
+		});
+	})
+})(jQuery);
